20dd7891582b92164e77369c812cd600062b0410
LajjanH <123291612+LajjanH@users.noreply.github.com>
2023-05-11 22:58:40 +0200

    Add files via upload

---
+++ Mobile_Escape_Room/cryptogram.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+  const rows = document.querySelectorAll(".row__1");
+  if (localStorage.getItem("DUSK")) {
+    for (let i = 0; i < rows.length; i++) {
+      rows[i].style.opacity = "1";
+    }
+  }
+};
+
+const inputCipher = document.getElementById("input__cipher");
+const BtnAnswer = document.getElementById("Btn__answer");
+const rows = document.querySelectorAll(".row__1");
+
+BtnAnswer.addEventListener("click", () => {
+  if (inputCipher.value.toUpperCase() === "DUSK") {
+    if ("vibrate" in navigator) {
+      function vibratePattern() {
+        navigator.vibrate([300, 100, 300, 100, 300]);
+      }
+      vibratePattern(); // call vibratePattern() function
+    }
+    for (let i = 0; i < rows.length; i++) {
+      rows[i].style.opacity = "1";
+    }
+    localStorage.setItem("DUSK", "true");
+  } else {
+    localStorage.removeItem("DUSK");
+    for (let i = 0; i < rows.length; i++) {
+      rows[i].style.opacity = "0";
+    }
+    if ("vibrate" in navigator) {
+      function vibrate() {
+        navigator.vibrate([1000]);
+      }
+      vibrate(); // call vibrate() function
+    }
+  }
+});
